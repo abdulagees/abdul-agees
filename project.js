ef34ae88f3064f56715016373e8de947669c417d
abdulagees <abdulagees2003@gmail.com>
2025-05-15 12:35:21 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,146 @@
+
+ function add1(){
+   window.open("https://www.fastrack.in/product/fastrack-kruz%2B-4.85-cm-super-hd-display-smart-watch-for-unisex-with-black-silicone-strap-38104pp01k.html?lang=en_IN&plp=true&catID=watches", )
+ }
+function add2(){
+   window.open("https://www.amazon.in/adidas-Resin-Originals-Unisex-Analog/dp/B0CJFYP43R/ref=sr_1_10?dib=eyJ2IjoiMSJ9.PMSMk6OVBwhgucOYMEvboGwZxJFkuAaGbcx4r9cpPIWfZ1OUZzmRnFp-lQnbeCFxqeFGC6iPvM8g5IYTzNnJGXrIax3tfOH_73TKUs8mlhZalmoIvcN1F8mItz_uGZljXJE6JAYWDM-RlmBQqRvXYZfETCvDc2J6j0oIxSbQ8X2QnAHAm-qVZkUCBCvVJXfHQ3kMpcyyLhxAFlKZ64PI4UStRHZEzBNT0O-lDvWvtVZfPD-mXWNH5NQoU69tKhhEVbarP5H72BnG-ULKEnX1YPT_5YDn33fB8YGYlhx_b0E.xMg8Y9KGSYnhZlViT_3jJ0tho8mABGKWA9OapL1Gz-M&dib_tag=se&qid=1747234942&refinements=p_89%3AAdidas&s=watches&sr=1-10")
+}
+function add3(){
+   window.open("https://www.amazon.in/adidas-Resin-Originals-Unisex-Analog/dp/B0CJFZ7XSZ/ref=sr_1_29?dib=eyJ2IjoiMSJ9.PMSMk6OVBwhgucOYMEvboGwZxJFkuAaGbcx4r9cpPIWfZ1OUZzmRnFp-lQnbeCFxqeFGC6iPvM8g5IYTzNnJGXrIax3tfOH_73TKUs8mlhZalmoIvcN1F8mItz_uGZljXJE6JAYWDM-RlmBQqRvXYZfETCvDc2J6j0oIxSbQ8X2QnAHAm-qVZkUCBCvVJXfHQ3kMpcyyLhxAFlKZ64PI4UStRHZEzBNT0O-lDvWvtVZfPD-mXWNH5NQoU69tKhhEVbarP5H72BnG-ULKEnX1YPT_5YDn33fB8YGYlhx_b0E.xMg8Y9KGSYnhZlViT_3jJ0tho8mABGKWA9OapL1Gz-M&dib_tag=se&qid=1747234942&refinements=p_89%3AAdidas&s=watches&sr=1-29")
+}
+function app1(){
+   window.open("https://www.apple.com/in/apple-watch-series-10/")
+}
+function app2(){
+   window.open("https://www.apple.com/in/watch/compare/?modelList=watch-se-gen1,watch-series-10,watch-ultra-2")
+}
+function app3(){
+   window.open("https://www.apple.com/in/watch/compare/?modelList=watch-se-gen1,watch-series-10,watch-ultra-2")
+}
+function fas1(){
+   window.open("https://www.fastrack.in/product/fastrack-marvellous-fx2-unisex-smart-watch-with-rose-gold-metal-strap-38148wm01.html?lang=en_IN&plp=true")
+}
+function fas2(){
+   window.open("https://www.fastrack.in/product/fastrack-tick-tock-quartz-analog-black-dial-watch-for-guys-with-black-stainless-steel-strap-3287km06.html?lang=en_IN&plp=true")
+}
+function fas3(){
+   window.open("https://www.fastrack.in/product/fastrack-quartz-analog-watch-with-brown-colour-strap-for-guys-3272sl03.html?lang=en_IN&plp=true")
+}
+ 
+
+{
+ let count = 0;
+function cart1(){
+  
+    count++;
+    document.getElementById('cart1').textContent =count;
+    window.alert("successfuly add to cart")
+    console.log
+  }
+  }
+
+
+  {
+ let count = 0;
+function cart2(){
+    
+    count++;
+    document.getElementById('cart2').textContent = count;
+    window.alert("successfuly add to cart")
+    console.log
+  }
+}
+
+{
+ let count=0;
+  function cart3(){
+     count++;
+     document.getElementById("cart3").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+
+{
+ let count=0;
+  function cart4(){
+     count++;
+     document.getElementById("cart4").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+{
+ let count=0;
+  function cart5(){
+     count++;
+     document.getElementById("cart5").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+
+{
+ let count=0;
+  function cart6(){
+     count++;
+     document.getElementById("cart6").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+
+
+{
+ let count=0;
+  function cart7(){
+     count++;
+     document.getElementById("cart7").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+
+{
+ let count=0;
+  function cart8(){
+     count++;
+     document.getElementById("cart8").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+
+{
+ let count=0;
+  function cart9(){
+     count++;
+     document.getElementById("cart9").textContent=count;
+     window.alert("cart added successfully")
+     console.log
+  }
+}
+
+function search(){
+   const query= document.getElementById("search").value.toLowerCase();
+
+   //// Match search term with ID of product
+   const product=document.getElementById(query);
+   if (product) {
+      product.scrollIntoView({behavior:"smooth"});
+   }
+   else{
+      alert("product not found")
+   }
+   console.log();
+   
+}
+
+
+
